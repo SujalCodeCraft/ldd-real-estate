e6197a41ac316f8d452995659dd177246c237ee2
nitinjha04 <nitinjha890@gmail.com>
2025-01-07 17:16:38 +0530

    api size minimize

--- api/baseApi.js
+++ api/baseApi.js
@@ -1,14 +1,1 @@
-// export const BASE_URL = "http://localhost:4000/api";
-export const BASE_URL = "https://ldd-backend.vercel.app/api";
-
-export function myFunction(
-  message = "Thank you! Your contact form has been submitted successfully."
-) {
-  console.log("toast");
-  var x = document.getElementById("snackbar");
-  x.innerText = message; // Set the dynamic message
-  x.className = "show";
-  setTimeout(function () {
-    x.className = x.className.replace("show", "");
-  }, 3000);
-}
+export const BASE_URL="https://ldd-backend.vercel.app/api";export function myFunction(e="Thank you! Your contact form has been submitted successfully."){console.log("toast");var t=document.getElementById("snackbar");t.innerText=e,t.className="show",setTimeout((function(){t.className=t.className.replace("show","")}),3e3)}
